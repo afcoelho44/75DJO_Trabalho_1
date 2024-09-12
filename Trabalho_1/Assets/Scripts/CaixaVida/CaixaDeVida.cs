18947b30fea1ee1977ece34e550448d29df45abe
Ana Fábia Coelho <afcoelho44@gmail.com>
2024-09-12 11:17:50 -0300

    Caixa de vida

---
+++ Trabalho_1/Assets/Scripts/CaixaVida/CaixaDeVida.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaixaDeVida : MonoBehaviour, IPegavel
+{
+    public void Pegar()
+    {
+        MovimentarPersonagem player = GameObject.FindWithTag("Player").GetComponent<MovimentarPersonagem>();
+        player.AtualizarVida(30);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
